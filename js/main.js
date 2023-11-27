d0dd744907901347355f2a29ea7b35a6fe389d94
facu <facumelieli@gmail.com>
2023-11-26 22:02:17 -0300

    guardar cambios

--- js/main.js
+++ js/main.js
@@ -1,35 +1,55 @@
 const tarifas = {
-    basico: 25000,
-    intermedio: 40000,
-    completo: 75000
-    };
-    function calcularCostoSeguro(edadConductor, tipoCobertura) {
-    let costoBase = tarifas[tipoCobertura];
+    basico: {
+        auto: 25000,
+        moto: 18000,
+        casa: 35000
+    },
+    intermedio: {
+        auto: 40000,
+        moto: 30000,
+        casa: 50000
+    },
+    completo: {
+        auto: 75000,
+        moto: 60000,
+        casa: 100000
+    }.auto6
+};
+
+function calcularCostoSeguro(edadConductor, tipoCobertura, tipoSeguro) {
+    let costoBase = tarifas[tipoCobertura][tipoSeguro];
     if (edadConductor < 30) {
         costoBase *= 1.2;
     }
     return costoBase;
-    }
+}
 
-    let precioTotal = 0;
-    let cotizando = true;
+let precioTotal = 0;
+let cotizando = true;
 
-    while (cotizando) {
-    const edadConductor = prompt("Por favor, ingrese su edad:");
-    const tipoCoberturaSeleccionado = prompt("Elija el tipo de cobertura (basico, intermedio o completo):");
-    const edadConductorNumero = parseInt(edadConductor);
-    if (!isNaN(edadConductorNumero) && tarifas[tipoCoberturaSeleccionado]) {
-    const costoSeguro = calcularCostoSeguro(edadConductorNumero, tipoCoberturaSeleccionado);
-    alert(`El costo del seguro es: $${costoSeguro}`);
-    precioTotal += costoSeguro;
+document.getElementById('cotizarBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const edadConductor = parseInt(document.getElementById('edadConductor').value);
+    const tipoCoberturaSeleccionado = document.getElementById('tipoCobertura').value;
+    const tipoSeguroSeleccionado = document.getElementById('tipoSeguro').value;
+    
+    if (!isNaN(edadConductor) && tarifas[tipoCoberturaSeleccionado] && tarifas[tipoCoberturaSeleccionado][tipoSeguroSeleccionado]) {
+        const costoSeguro = calcularCostoSeguro(edadConductor, tipoCoberturaSeleccionado, tipoSeguroSeleccionado);
+        alert(`El costo del seguro es: $${costoSeguro}`);
+        precioTotal += costoSeguro;
+        alert(`El costo total de los seguros cotizados es: $${precioTotal}`);
+
+        // Preguntarle al usuario si quiere seguir corizando
+        const continuar = prompt("¿Desea cotizar otro seguro? (Sí/No)").toLowerCase();
+        if (continuar !== "si" && continuar !== "sí") {
+            cotizando = false;
+        }
     } else {
-    alert("Edad ingresada no válida o tipo de cobertura incorrecto. Por favor, ingrese valores válidos.");
+        alert("Edad ingresada no válida o tipo de cobertura incorrecto. Por favor, ingrese valores válidos.");
     }
-    const continuar = prompt("¿Desea cotizar otro seguro? (Sí/No)").toLowerCase();
-    if (continuar !== "si" && continuar !== "sí") {
-    cotizando = false;
+    if (!cotizando) {
+        alert(`El costo total de todos los seguros cotizados es: $${precioTotal}`);
     }
-    }
-
-    alert(`El precio total de los seguros cotizados es: $${precioTotal}`);
+});
 
